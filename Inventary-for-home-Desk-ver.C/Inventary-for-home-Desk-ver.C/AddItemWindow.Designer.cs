9ba0efbcac46889aa84c4672ffdfadd74a67ecd8
Yazbeth <yazryrg@outlook.com>
2024-06-26 22:58:18 -0600

    +Corrección de errores

--- Inventary-for-home-Desk-ver.C/Inventary-for-home-Desk-ver.C/AddItemWindow.Designer.cs
+++ Inventary-for-home-Desk-ver.C/Inventary-for-home-Desk-ver.C/AddItemWindow.Designer.cs
@@ -41,12 +41,24 @@
             dateTimePicker1 = new DateTimePicker();
             dateTimePicker2 = new DateTimePicker();
             textBox2 = new TextBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(6, 3);
             label1.Name = "label1";
             label1.Size = new Size(292, 15);
             label1.TabIndex = 0;
@@ -55,7 +67,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 38);
+            label2.Location = new Point(14, 29);
             label2.Name = "label2";
             label2.Size = new Size(113, 15);
             label2.TabIndex = 1;
@@ -64,7 +76,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 82);
+            label3.Location = new Point(15, 73);
             label3.Name = "label3";
             label3.Size = new Size(119, 15);
             label3.TabIndex = 2;
@@ -73,7 +85,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 126);
+            label4.Location = new Point(15, 117);
             label4.Name = "label4";
             label4.Size = new Size(116, 15);
             label4.TabIndex = 3;
@@ -82,7 +94,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(12, 170);
+            label5.Location = new Point(15, 161);
             label5.Name = "label5";
             label5.Size = new Size(98, 15);
             label5.TabIndex = 4;
@@ -91,7 +103,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(12, 214);
+            label6.Location = new Point(15, 205);
             label6.Name = "label6";
             label6.Size = new Size(112, 15);
             label6.TabIndex = 5;
@@ -99,21 +111,21 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 56);
+            textBox1.Location = new Point(15, 47);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(200, 23);
             textBox1.TabIndex = 8;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(12, 144);
+            textBox3.Location = new Point(15, 135);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(200, 23);
             textBox3.TabIndex = 10;
             // 
             // button1
             // 
-            button1.Location = new Point(12, 297);
+            button1.Location = new Point(250, 358);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 13;
@@ -122,7 +134,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(93, 297);
+            button2.Location = new Point(331, 358);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 14;
@@ -132,47 +144,139 @@
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(12, 188);
+            dateTimePicker1.Location = new Point(15, 179);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(200, 23);
             dateTimePicker1.TabIndex = 15;
             // 
             // dateTimePicker2
             // 
-            dateTimePicker2.Location = new Point(12, 232);
+            dateTimePicker2.Location = new Point(15, 223);
             dateTimePicker2.Name = "dateTimePicker2";
             dateTimePicker2.Size = new Size(200, 23);
             dateTimePicker2.TabIndex = 16;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(12, 100);
+            textBox2.Location = new Point(15, 91);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(200, 23);
             textBox2.TabIndex = 17;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(27, 37);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(379, 294);
+            tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dateTimePicker2);
+            tabPage1.Controls.Add(dateTimePicker1);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(371, 266);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(textBox5);
+            tabPage2.Controls.Add(textBox4);
+            tabPage2.Controls.Add(label10);
+            tabPage2.Controls.Add(label9);
+            tabPage2.Controls.Add(label8);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(371, 266);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(223, 15);
+            label7.TabIndex = 19;
+            label7.Text = "Seleccione el elemento que desea añadir:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(6, 3);
+            label8.Name = "label8";
+            label8.Size = new Size(361, 15);
+            label8.TabIndex = 0;
+            label8.Text = "Introduzca los datos requeridos para el nuevo valor de importancia:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(15, 39);
+            label9.Name = "label9";
+            label9.Size = new Size(154, 15);
+            label9.TabIndex = 1;
+            label9.Text = "Nuevo valor de importancia";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(15, 83);
+            label10.Name = "label10";
+            label10.Size = new Size(139, 15);
+            label10.TabIndex = 2;
+            label10.Text = "Introdusca la descripción";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(15, 57);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(15, 101);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 4;
             // 
             // AddItemWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(330, 332);
-            Controls.Add(textBox2);
-            Controls.Add(dateTimePicker2);
-            Controls.Add(dateTimePicker1);
+            ClientSize = new Size(417, 393);
+            Controls.Add(label7);
+            Controls.Add(tabControl1);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox1);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "AddItemWindow";
             Text = "Form2";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -192,5 +296,14 @@
         private DateTimePicker dateTimePicker1;
         private DateTimePicker dateTimePicker2;
         private TextBox textBox2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
     }
 }
