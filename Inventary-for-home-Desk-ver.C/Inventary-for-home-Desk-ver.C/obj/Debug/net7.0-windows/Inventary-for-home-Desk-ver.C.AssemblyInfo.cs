c69d86a35b2ea90af6deb9e9cb116953e74d64db
Yazbeth <yazryrg@outlook.com>
2024-07-22 23:48:21 -0600

    +I

--- Inventary-for-home-Desk-ver.C/Inventary-for-home-Desk-ver.C/obj/Debug/net7.0-windows/Inventary-for-home-Desk-ver.C.AssemblyInfo.cs
+++ Inventary-for-home-Desk-ver.C/Inventary-for-home-Desk-ver.C/obj/Debug/net7.0-windows/Inventary-for-home-Desk-ver.C.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventary-for-home-Desk-ver.C")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+761b3a17a2017c6b63670e6fe342c5e5704e98a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6152f3d57452ad0edd288fb391446b309b027b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventary-for-home-Desk-ver.C")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventary-for-home-Desk-ver.C")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
